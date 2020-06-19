adea39a289904f5b8f32308d7aba12fff7347dae
Rohit Kulkarni <rht_kulkarni@outlook.com>
2020-06-19 07:03:20 +0000

    VSCode Settings

---
+++ VSCodeExtensions.ps1
@@ -0,0 +1,23 @@
+# Extensions for Visual Studio
+# C#
+code --install-extension ms-dotnettools.csharp
+# SQL Server (mssql)
+code --install-extension ms-mssql.mssql
+# Azure Tool Extension
+code --install-extension ms-vscode.vscode-node-azure-pack
+# Powershell Extension
+code --install-extension ms-vscode.PowerShell
+#GitLens — Git supercharged
+code --install-extension eamodio.gitlens
+# gitignore
+code --install-extensioncodezombiech.gitignore
+# Material Theme
+code --install-extension equinusocio.vsc-material-theme
+# Material Icon Theme
+code --install-extension pkief.material-icon-theme
+# Bracket Pair Colorizer 2
+code --install-extension coenraads.bracket-pair-colorizer-2
+# indent-rainbow
+code --install-extension oderwat.indent-rainbow
+# YAML
+code --install-extension redhat.vscode-yaml
